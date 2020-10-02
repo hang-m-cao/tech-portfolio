3fcdf12524b54037cbc7bd10379e835c4c5cca1b
Hang Cao <67395310+hang-m-cao@users.noreply.github.com>
2020-10-02 12:22:05 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -14,7 +14,7 @@ const users = [];
 const siteConfig = {
     title: 'Hang Cao', // Title for your website.
     tagline: '1st year computer science major at UCI.',
-    profilePhoto: 'img/profile.png',
+    profilePhoto: 'img/profile.JPG',
     url: 'https://hang-m-cao.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
