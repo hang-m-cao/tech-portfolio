6800df2c5250bb70045451be7a397e50aaece7fa
Hang Cao <67395310+hang-m-cao@users.noreply.github.com>
2020-10-02 12:19:37 -0700

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,10 +12,10 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Hang Cao', // Title for your website.
+    tagline: '1st year computer science major at UCI.',
     profilePhoto: 'img/profile.png',
-    url: 'https://joatmon08.github.io', // Your website URL
+    url: 'https://hang-m-cao.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'hang-m-cao',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
@@ -44,11 +44,11 @@ const siteConfig = {
     /* Colors for website */
     colors: {
         primaryColor: '#2C312E',
-        secondaryColor: '#72C3E7',
+        secondaryColor: '#BF8DF2',
     },
 
     // This copyright info is used in /core/Footer.js and blog RSS/Atom feeds.
-    copyright: `Copyright © ${new Date().getFullYear()} Your Name Here`,
+    copyright: `Copyright © ${new Date().getFullYear()} Hang Cao`,
 
     highlight: {
         // Highlight.js theme to use for syntax highlighting in code blocks.
@@ -67,9 +67,9 @@ const siteConfig = {
     enableUpdateTime: true,
 
     socialMedia: {
-        linkedin: '',
+        linkedin: 'https://www.linkedin.com/in/hang-m-cao/',
         twitter: '',
-        github: '',
+        github: 'https://github.com/hang-m-cao/',
     },
 
     // You may provide arbitrary config keys to be used as needed by your
