0cb9dffc4134105c1a11702c1604d0070a9187ef
Hang Cao <67395310+hang-m-cao@users.noreply.github.com>
2020-10-10 18:11:31 -0700

    fix base url

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -16,7 +16,7 @@ const siteConfig = {
     tagline: '1st year computer science major at UCI.',
     profilePhoto: 'img/profile.png',
     url: 'https://hang-m-cao.github.io', // Your website URL
-    baseUrl: '/', // Base URL for your project */
+    baseUrl: '/tech-portfolio/', // Base URL for your project */
     // For github.io type URLs, you would set the url and baseUrl like:
     //   url: 'https://facebook.github.io',
     //   baseUrl: '/test-site/',
